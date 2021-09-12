5f09da5399701f9910fb81447567f6294981456e
BabyGoFast <spectraserveurs@gmail.com>
2021-09-12 14:00:11 +0200

    script admin

---
+++ config.lua
@@ -0,0 +1,29 @@
+print("^0======================================================================^7")
+print("^0[^6Author^0] ^1:^0 ^1BabyLua^1")
+print("^0[^6Version^0] ^1:^0 ^02.0^1")
+print("^0======================================================================^7")
+
+Config = {}
+Config.Locale = 'en'
+
+-- Rank pour reports notification
+
+Config.adminRanks = {
+				'owner',
+				'dev',
+				'superadmin',
+				'admin',
+				'mod',
+}
+
+-- Logs Give Money
+
+discord_webhook = {
+	url = ""
+}
+
+-- Logs HRP - Activation NOM / blips / et tout ce qui n'est pas à faire en RP
+
+discord_webhookhrp = {
+	url = ""
+}
